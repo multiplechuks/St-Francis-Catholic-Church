25560f7fcdcf8125239a2b2ac387c0c403d9ab2a
Ezekwe Johnbosco <multiplechuks@outlook.com>
2018-09-10 18:29:45 +0100

    Add project files.

---
+++ StFrancisChurch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StFrancisChurch.Startup))]
+namespace StFrancisChurch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
